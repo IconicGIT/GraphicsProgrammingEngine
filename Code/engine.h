e0e3443bc2702a755eb3bc6657bd57348c03c934
IconicGIT <72080813+IconicGIT@users.noreply.github.com>
2024-04-08 16:25:03 +0200

    Initial commit

---
+++ Code/engine.h
@@ -0,0 +1,202 @@
+//
+// engine.h: This file contains the types and functions relative to the engine.
+//
+
+#pragma once
+
+#include "platform.h"
+#include <glad/glad.h>
+
+typedef glm::vec2  vec2;
+typedef glm::vec3  vec3;
+typedef glm::vec4  vec4;
+typedef glm::ivec2 ivec2;
+typedef glm::ivec3 ivec3;
+typedef glm::ivec4 ivec4;
+
+struct Image
+{
+    void* pixels;
+    ivec2 size;
+    i32   nchannels;
+    i32   stride;
+};
+
+struct Texture
+{
+    GLuint      handle;
+    std::string filepath;
+};
+
+
+
+struct VertexShaderAtt
+{
+    u8 location;
+    u8 componentCount;
+};
+
+struct VertexShaderLayout
+{
+    std::vector<VertexShaderAtt> attributes;
+};
+
+struct Program
+{
+    GLuint             handle;
+    std::string        filepath;
+    std::string        programName;
+    u64                lastWriteTimestamp; // What is this for?
+    VertexShaderLayout vertexShaderLayout;
+    
+};
+
+enum Mode
+{
+    Mode_TexturedQuad,
+    Mode_Count
+};
+
+struct Model
+{
+    u32 meshIdx;
+    std::vector<u32> materialIdx;
+};
+
+struct Vao
+{
+    GLuint handle;
+    GLuint programHandle;
+};
+
+struct VertexBufferAtt
+{
+    u8 location;
+    u8 compCount;
+    u8 offset;
+};
+
+struct VertexBuffLayout
+{
+    std::vector<VertexBufferAtt> attributes;
+    u8 stride;
+};
+
+
+struct Submesh
+{
+    VertexBuffLayout vertexBuffLayout;
+    std::vector<float> vertices;
+    std::vector<u32> indices;
+    u32 vertexOffset;
+    u32 indexOffset;
+
+    std::vector<Vao> vaos;
+};
+
+struct Mesh
+{
+    std::vector<Submesh> submeshes;
+    GLuint vertexBufferHandle;
+    GLuint indexBufferHandle;
+};
+
+struct Material
+{
+    std::string name;
+    vec3 albedo;
+    vec3 emissive;
+    f32 smoothness;
+    u32 albedoTextureIdx;
+    u32 emissiveTextureIdx;
+    u32 specularTextureIdx;
+    u32 normalsTextureIdx;
+    u32 bumpTextureIdx;
+};
+
+struct App
+{
+    // Loop
+    f32  deltaTime;
+    bool isRunning;
+
+    // Input
+    Input input;
+
+    // Graphics
+    char gpuName[64];
+    char openGlVersion[64];
+
+    ivec2 displaySize;
+
+    std::vector<Texture>  textures;
+    std::vector<Program>  programs;
+    std::vector<Model> models;
+    std::vector<Material> materials;
+    std::vector<Mesh> meshes;
+
+    // program indices
+    u32 texturedGeometryProgramIdx;
+    
+    // texture indices
+    u32 diceTexIdx;
+    u32 whiteTexIdx;
+    u32 blackTexIdx;
+    u32 normalTexIdx;
+    u32 magentaTexIdx;
+
+    // Mode
+    Mode mode;
+
+    // Embedded geometry (in-editor simple meshes such as
+    // a screen filling quad, a cube, a sphere...)
+    GLuint embeddedVertices;
+    GLuint embeddedElements;
+
+    // Location of the texture uniform in the textured quad shader
+    GLuint programUniformTexture;
+
+    // VAO object to link our screen filling quad with our textured quad shader
+    GLuint vao;
+
+};
+
+struct VertexV3V2
+{
+    glm::vec3 pos;
+    glm::vec2 uv;
+};
+
+
+
+
+void Init(App* app);
+
+void Gui(App* app);
+
+void Update(App* app);
+
+void Render(App* app);
+
+class ErrorGuardOGL
+{
+public:
+    ErrorGuardOGL(const char *message) : msg(message)
+    {
+        CheckGLError("BEGIN", msg);
+    }
+
+    ~ErrorGuardOGL()
+    {
+        CheckGLError("END", msg);
+
+    }
+
+    static void CheckGLError(const char* around, const char* message);
+
+    const char* msg;
+
+};
+
+
+
